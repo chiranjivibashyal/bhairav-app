8d92c1df9b82983bedfeb2ac7ae83a5c900d2d4d
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2021-11-28 17:54:55 +1100

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,88 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Jug Kettle',
+            category: 'Kitchen items',
+            image: '/images/p1.jpg',
+            price: 12,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: ' Thermos Bottle',
+            category: 'Kitchen items',
+            image: '/images/p2.jpg',
+            price: 15,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High quality product'
+        }, {
+            _id: '3',
+            name: 'Coffee Pot',
+            category: 'Kitchen items',
+            image: '/images/p3.jpg',
+            price: 10,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'High quality product'
+        }, {
+            _id: '4',
+            name: 'Hot Case',
+            category: 'Kitchen items',
+            image: '/images/p4.jpg',
+            price: 10,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'High quality product'
+        }, {
+            _id: '5',
+            name: 'Whistling Kettle',
+            category: 'Kitchen items',
+            image: '/images/p5.jpg',
+            price: 15,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        }, {
+            _id: '6',
+            name: 'Lunch Box',
+            category: 'Kitchen items',
+            image: '/images/p6.jpg',
+            price: 12,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        }, {
+            _id: '7',
+            name: 'Travel Mug',
+            category: 'Kitchen items',
+            image: '/images/p7.jpg',
+            price: 8,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        }, {
+            _id: '8',
+            name: 'Hot Jug',
+            category: 'Kitchen items',
+            image: '/images/p8.jpg',
+            price: 8,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+    ],
+};
+
+export default data;
